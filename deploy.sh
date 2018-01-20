4baf933057156783bb617e131b8cb4a248190263
Hung-I Chiu <i314i@yahoo.com.tw>
2018-01-20 08:25:02 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [ -d ".deploy_git/$1" ] && [ -n "$1" ]; then
     undeploy=$(cat .deployignore | xargs | sed "s: :\|:g; s:\.:\\\.:g;")'|^\.$|^\.\.$'
-    ls -a | grep -Ev $undeploy | xargs -I % sh  -c "cp -r % .deploy_git/$1/"
+    ls -a | grep -Ev $undeploy | grep -Ev .git | xargs -I % sh  -c "cp -r % .deploy_git/$1/"
     cd .deploy_git/$1
     git add .
     git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
